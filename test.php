7025dd1342b3b706d808ed6c31a00e14d0fbfb9b
rsanlazaro <rsanlazaro@hotmail.com>
2024-09-05 16:14:03 -0600

    test 2

--- test.php
+++ test.php
@@ -6,6 +6,7 @@ $conn = connectDB();
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     echo "FIRST OK";
+    var_dump($_FILES);
     if (isset($_FILES['image-1']['name'])) {
         $file = $_FILES['image-1']['name'];
         $path = pathinfo($file);
