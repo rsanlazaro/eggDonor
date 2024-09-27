da1f61371a4fd4f9610d3782214486a9cce7ee78
rsanlazaro <rsanlazaro@hotmail.com>
2024-09-27 13:59:04 -0600

    test 2

--- testDelete.php
+++ testDelete.php
@@ -10,7 +10,7 @@ $rawData = file_get_contents('php://input');
 
 // Decode the JSON into a PHP associative array
 $data = json_decode($rawData, true);
-$twoElementsArr = explode("/", $var);
+$twoElementsArr = explode("/", $data);
 $twoElements = array_slice($twoElementsArr,-2);
 $twoElementsExt = explode(".", $twoElements[1]);
 $deleteTgt = $twoElements[0] . "/" . $twoElementsExt[0]; 
@@ -23,7 +23,7 @@ $deleteTgt = $twoElements[0] . "/" . $twoElementsExt[0];
 // if (($_FILES['image-1']['size']) > 0) {
 $result = $cloudinary->uploadApi()
 ->destroy($deleteTgt);
-// var_dump($result);
+var_dump($result);
 //     $json  = json_encode($result);
 //     $array = json_decode($json, true);
 //     $secureUrl = $array['secure_url'];
