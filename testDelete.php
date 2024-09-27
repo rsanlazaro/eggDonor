fc92996670675cee98c17b4a66864df35cad0892
rsanlazaro <rsanlazaro@hotmail.com>
2024-09-27 13:57:48 -0600

    test 2

--- testDelete.php
+++ testDelete.php
@@ -4,14 +4,16 @@ session_start();
 
 include "includes/app.php";
 $conn = connectDB();
-var_dump($_POST);
 
 // Get the raw POST body
 $rawData = file_get_contents('php://input');
 
 // Decode the JSON into a PHP associative array
 $data = json_decode($rawData, true);
-var_dump($data);
+$twoElementsArr = explode("/", $var);
+$twoElements = array_slice($twoElementsArr,-2);
+$twoElementsExt = explode(".", $twoElements[1]);
+$deleteTgt = $twoElements[0] . "/" . $twoElementsExt[0]; 
 
 // if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 //     if (isset($_FILES['image-1']['name'])) {
@@ -19,13 +21,13 @@ var_dump($data);
 //         $path = pathinfo($file);
 //         $_FILES['image-1']['name'] = $id . "_1";
 // if (($_FILES['image-1']['size']) > 0) {
-// $result = $cloudinary->uploadApi()
-// ->destroy('test/_1');
+$result = $cloudinary->uploadApi()
+->destroy($deleteTgt);
 // var_dump($result);
 //     $json  = json_encode($result);
 //     $array = json_decode($json, true);
 //     $secureUrl = $array['secure_url'];
-//     header("Location: test.php");
+header("Location: test.php?msg=La imagen ha sido borrada exitosamente");
 // }
 //     }
 // }
