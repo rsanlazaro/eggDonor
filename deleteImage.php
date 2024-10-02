fcade162e1387304bb9f6370dbbaf7fd45d5f65f
rsanlazaro <rsanlazaro@hotmail.com>
2024-10-01 20:29:39 -0600

    test 2

--- deleteImage.php
+++ deleteImage.php
@@ -8,7 +8,12 @@ $data = json_decode($rawData, true);
 $twoElementsArr = explode("/", $data);
 $twoElements = array_slice($twoElementsArr,-2);
 $twoElementsExt = explode(".", $twoElements[1]);
-$deleteTgt = $twoElements[0] . "/" . $twoElementsExt[0]; 
+$deleteTgt = $twoElements[0] . "/" . $twoElementsExt[0];
+$index = substr($deleteTgt,-1);
+$cellElement = "ext_img_" + $index;
+$idElement = substr($deleteTgt,-6,4);
+$query = "UPDATE donants SET ${cellElement} = NULL WHERE id = ${idElement}";
+$result = mysqli_query($conn, $query); 
 $apiResponse = $cloudinary->uploadApi()->destroy($deleteTgt,$options = [
     "media_metadata" => true
 ]);
