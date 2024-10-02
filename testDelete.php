0de849f2f7f74b1590329be2c81eb5c62e623f30
rsanlazaro <rsanlazaro@hotmail.com>
2024-10-01 19:52:03 -0600

    test 2

--- testDelete.php
+++ testDelete.php
@@ -1,41 +1,24 @@
 <?php
-
 session_start();
-
 include "includes/app.php";
 $conn = connectDB();
-
-// Get the raw POST body
 $rawData = file_get_contents('php://input');
-
-// Decode the JSON into a PHP associative array
 $data = json_decode($rawData, true);
 $twoElementsArr = explode("/", $data);
 $twoElements = array_slice($twoElementsArr,-2);
 $twoElementsExt = explode(".", $twoElements[1]);
 $deleteTgt = $twoElements[0] . "/" . $twoElementsExt[0]; 
-
-// if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-//     if (isset($_FILES['image-1']['name'])) {
-//         $file = $_FILES['image-1']['name'];
-//         $path = pathinfo($file);
-//         $_FILES['image-1']['name'] = $id . "_1";
-// if (($_FILES['image-1']['size']) > 0) {
 $apiResponse = $cloudinary->uploadApi()->destroy($deleteTgt,$options = [
     "media_metadata" => true
 ]);
-// var_dump($apiResponse);
 $response = $apiResponse->offsetGet('result');
+if (headers_sent()) {
+    die("Headers have already been sent!");
+}
 if ($response == "ok") {
     header("Location: test.php?msg=La imagen ha sido borrada exitosamente.");
+    exit;
 } else {
     header("Location: test.php?msg=Ocurrió un problema con la eliminación de la imagen. Por favor, intente de nuevo.");
+    exit;
 }
-// var_dump($result);
-//     $json  = json_encode($result);
-//     $array = json_decode($json, true);
-//     $secureUrl = $array['secure_url'];
-// header("Location: test.php?msg=La imagen ha sido borrada exitosamente");
-// }
-//     }
-// }
