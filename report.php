6d633ef5f1baffe19ab501dc2089c03d0b1aa7e3
rsanlazaro <rsanlazaro@hotmail.com>
2024-11-22 22:10:14 -0600

    PDF fenotipe

--- report.php
+++ report.php
@@ -40,6 +40,11 @@ $imageData2 = base64_encode(file_get_contents($imageUrl2));
 $imageData3 = base64_encode(file_get_contents($imageUrl3));
 $imageData4 = base64_encode(file_get_contents($imageUrl4));
 
+var_dump($imageData1);
+var_dump($imageData2);
+var_dump($imageData3);
+var_dump($imageData4);
+
 $html1 = '
 <!DOCTYPE html>
 <html lang="en">
@@ -869,4 +874,4 @@ foreach ($htmlChunks as $chunk) {
 // $mpdf->Image($imageUrl, 10, 50, 100, 100); // (URL, x, y, width, height)
 
 // Output PDF
-$mpdf->Output('sample.pdf', 'I'); // D = force download, I = inline display, F = save to file
+// $mpdf->Output('sample.pdf', 'I'); // D = force download, I = inline display, F = save to file
